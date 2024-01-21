2e6d6f8e604a7727a37add82eb7c184c26f78e7d
s2quake <han0210@netsgo.com>
2024-01-21 22:33:32 +0900

    build: Fixed an issue that caused dotnet pack command to fail.

--- build.ps1
+++ build.ps1
@@ -2,9 +2,15 @@ param(
     [string]$OutputPath = "",
     [string]$Framework = "net8.0",
     [string]$KeyPath = "",
-    [string]$LogPath = ""
+    [string]$LogPath = "",
+    [switch]$Pack
 )
 
 $solutionPath = Join-Path $PSScriptRoot "communication.sln" -Resolve
-$buildFile = Join-Path $PSScriptRoot "build" "build.ps1" -Resolve
-& $buildFile $solutionPath -Publish -KeyPath $KeyPath -Sign -OutputPath $OutputPath -Framework $Framework -LogPath $LogPath
+$buildFile = Join-Path $PSScriptRoot ".build" "build.ps1" -Resolve
+if ($Pack) {
+    & $buildFile $solutionPath -Pack -KeyPath "$KeyPath" -Sign -OutputPath "$OutputPath" -LogPath "$LogPath"
+}
+else {
+    & $buildFile $solutionPath -Publish -KeyPath $KeyPath -Sign -OutputPath $OutputPath -Framework $Framework -LogPath $LogPath
+}
