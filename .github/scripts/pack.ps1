147f9a666ecbb14d5e2446831599da5c4edb5cb2
s2quake <han0210@netsgo.com>
2024-06-29 15:53:35 +0900

    ci: Add pack action.

---
+++ .github/scripts/pack.ps1
@@ -0,0 +1,35 @@
+#!/usr/local/bin/pwsh
+param (
+    [ValidateScript({ ($_ -eq "") -or !(Test-Path $_ -PathType Leaf) })]
+    [string]$OutputPath = "",
+    [ValidateScript({ $_ -ge 0 })]
+    [int]$PullRequestNumber = 0,
+    [ValidateScript({ ($_ -eq "") -or (Test-Path $_) })]
+    [string]$KeyPath = ""
+)
+
+$namespaces = @{
+    ns = "http://schemas.microsoft.com/developer/msbuild/2003"
+}
+$propsPath = "Directory.Build.props"
+$fileVersionPath = "/ns:Project/ns:PropertyGroup/ns:FileVersion"
+$result = Select-Xml -Path $propsPath -Namespace $namespaces -XPath $fileVersionPath
+if ($null -eq $result) {
+    Write-Host "File version not found"
+    exit 1
+}
+
+$fileVersion = $result.Node.InnerText
+$KeyPath = $KeyPath ? $(Resolve-Path -Path $KeyPath) : ""
+$OutputPath = $OutputPath ? [System.IO.Path]::GetFullPath($OutputPath) : ""
+$keyPathExits = Test-Path -Path $KeyPath
+
+$options = @(
+    $OutputPath ? "-o '$OutputPath'" : ""
+    "-p:FileVersion='$fileVersion'"
+    $PullRequestNumber ? "--version-suffix pr.$PullRequestNumber" : ""
+    $keyPathExits ? "-p:TreatWarningsAsErrors=true" : ""
+    $keyPathExits ? "-p:AssemblyOriginatorKeyFile='$KeyPath'" : ""
+) | Where-Object { $_ }
+
+Invoke-Expression -Command "dotnet pack $($options -join " ")"
